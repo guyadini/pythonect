06f9458cfe404c28cc941c9a1c0d7c4d83fcf2d4
guyadini <guyadini@gmail.com>
2012-09-10 10:30:38 +0300

    Continuing server. Wrapping currently broken.

--- pythonect/internal/server.py
+++ pythonect/internal/server.py
@@ -1,8 +1,31 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 import xmlrpclib
+import multiprocessing
 
 
 POLL_INTERVAL = 0.1
+
+def start_xml_server(port):
+    # Create server
+    server = SimpleXMLRPCServer(("localhost", port) )
+    server.register_introspection_functions()
+    
+    # Register all builtins in the xml server:
+    
+    print dir(__builtins__)
+    for builtin_name in dir(__builtins__):
+        try:
+            func = eval(builtin_name)
+            server.register_function(func, builtin_name)
+            print 'registered %s successfully' % builtin_name
+        except Exception, e:
+            print '*****Failed to eval %s' % builtin_name
+        
+    
+    # Run the server's main loop
+    server.serve_forever()
+    
+
 
 
 class _DistServer(object):
@@ -14,18 +37,21 @@ class _DistServer(object):
     def __init__(self, server_name=None, port=8092):
         if server_name:
             try:
-                self.server = SimpleXMLRPCServer(server_name)
+                # TODO: how do we verify that the server exists and is reachable?
+                self.server = 'external'
+                self.client = xmlrpclib.ServerProxy(server_name)
             except Exception, e:
                 print 'Server name %s unsuported, exiting' % server_name
                 exit(1)
 
         else:
             print '==== Starting Pythonect development server ===='
-            self.server = __start_server_process(port)
+            self.server = self.__start_server_process(port)
+            self.client = xmlrpclib.ServerProxy('http://localhost:%d' % port)
+            print 'Methods: ', self.client.system.listMehods()
+            print 'Testing server: max(3,4) = %d' % self.client.max(3,4)
 
-        #TODO: register functions
-
-        self.server.serve_forever(poll_interval=POLL_INTERVAL)
+        
 
     # TODO - implement, wrap all python builtins. Cool!
 
@@ -33,5 +59,8 @@ class _DistServer(object):
         '''
         Start xml-rpc server in a new process.
         '''
+        proc = multiprocessing.Process(target=start_xml_server, args=(port,))
+        proc.start()
+        
 
-        raise Exception('TODO: implement, i.e. http://docs.python.org/library/simplexmlrpcserver.html#SimpleXMLRPCServer.CGIXMLRPCRequestHandler')
+        
